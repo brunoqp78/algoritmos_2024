cbe5c42340cd33210c160f67e16d5416f0ec2c55
bruno queiroz <brunoqp78@gmail.com>
2024-09-26 20:19:00 -0300

    Exercício recursivo Fibonacci dia 20 de setembro de 2024

---
+++ recursividade/Fibonacci.java
@@ -0,0 +1,24 @@
+package recursividade;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Digite a posição da série de fibonacci?");
+        int n = s.nextInt();
+        int r = calcularFib(n);  
+        System.out.println("O valor da posição " + n + " é " + r);  
+        s.close();    
+    }
+
+    public static int calcularFib(int n){
+       if (n==0){
+            return 0;
+       }else if (n==1){
+            return 1;
+       }else{
+            return calcularFib(n-1) + calcularFib(n-2);
+       }
+    }
+}
